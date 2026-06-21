34983564a09ac81ec8330d64e9cf1710c3064aaa
sv_khuong_vnn <khuongvo2105@gmail.com>
2026-06-21 16:06:21 +0700

    refactor(model): remove connector model

--- ShopDraw/ShopDraw/Models/3D/CurveModel.cs
+++ ShopDraw/ShopDraw/Models/3D/CurveModel.cs
@@ -6,5 +6,12 @@
         public string FamilyName { get; set; }
         public string TypeName { get; set; }
         public string LevelName { get; set; }
+        public RvtXYZ StartPoint { get; set; }
+        public RvtXYZ EndPoint { get; set; }
+        public double Diameter { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public string StartConnectedToId { get; set; }
+        public string EndConnectedToId { get; set; }
     }
 }
--- ShopDraw/ShopDraw/Models/3D/FittingModel.cs
+++ ShopDraw/ShopDraw/Models/3D/FittingModel.cs
@@ -1,18 +1,14 @@
-﻿using Autodesk.Revit.DB;
-using System.Collections.Generic;
-
-namespace ShopDraw.Models._3D
+﻿namespace ShopDraw.Models._3D
 {
     internal class FittingModel
     {
         public string Id { get; set; }
+        public string FamilyName { get; set; }
+        public string TypeName { get; set; }
+        public string LevelName { get; set; }
         public RvtXYZ Origin { get; set; }
         public RvtXYZ BasisX { get; set; }
         public RvtXYZ BasisY { get; set; }
         public RvtXYZ BasisZ { get; set; }
-        public string FamilyName { get; set; }
-        public string TypeName { get; set; }
-        public string LevelName { get; set; }
-        public List<ConnectorModel> Connectors { get; set; }
     }
 }
