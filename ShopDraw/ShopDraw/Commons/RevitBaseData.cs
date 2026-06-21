521d4cd042a3a1b18a140662a9d9549e9c9d2df6
sv_khuong_vnn <khuongvo2105@gmail.com>
2026-06-21 09:27:35 +0700

    feat: add RevitBaseData

---
+++ ShopDraw/ShopDraw/Commons/RevitBaseData.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ShopDraw.Commons
+{
+    public class RevitBaseData
+    {
+        public static Application Application { get; set; }
+        public static UIApplication UIApplication { get; set; }
+        public static UIDocument UIDocument { get; set; }
+        public static Document Document { get; set; }
+
+        public static void Init(UIDocument uidoc)
+        {
+            UIDocument = uidoc;
+            Document = uidoc.Document;
+            Application = uidoc.Application.Application;
+            UIApplication = uidoc.Application;
+        }
+    }
+}
